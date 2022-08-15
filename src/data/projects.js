1ae14ea37ec9b7205a23a0d31172d2d3ce7b23b5
Diego-1D <diego1d_fernandes@outlook.com>
2022-08-15 16:47:41 -0300

    feat: (projects data) inserts project information

---
+++ src/data/projects.js
@@ -0,0 +1,84 @@
+//IMAGES
+import LinkenInImage from '../Images/linkedIn.png';
+import AnimeImage from '../Images/anime.png';
+import TheCocktailImage from '../Images/theCocktail.png';
+import FinancesControlImage from '../Images/financesControl.png';
+import IceCreamImage from '../Images/iceCream.png';
+import FitSpaImage from '../Images/fitSpa.png';
+
+//ICONS
+import FirebaseIcon from '../Images/Icons/firebase.svg';
+import ReactIcon from '../Images/Icons/react.svg';
+import CssIcon from '../Images/Icons/css.svg';
+import AntDesignIcon from '../Images/Icons/antDesign.svg';
+import StyledComponentsIcon from '../Images/Icons/styledComponents.svg';
+import SassIcon from '../Images/Icons/sass.svg';
+import TypeScriptIcon from '../Images/Icons/typescript.svg';
+import BootStrapIcon from '../Images/Icons/bootstrap.png'
+
+export const Projects = [
+    {
+        id: 1,
+        image: LinkenInImage,
+        name: 'Clone do LinkedIn',
+        technoUsed: [
+            ReactIcon,
+            CssIcon,
+            FirebaseIcon,
+        ],
+        url: 'https://diego-1d.github.io/linkedIn-clone/'
+    },
+    {
+        id: 2,
+        image: AnimeImage,
+        name: 'Anime',
+        technoUsed: [
+            ReactIcon,
+            StyledComponentsIcon
+        ],
+        url: 'https://diego-1d.github.io/anime/'
+    },
+    {
+        id: 3,
+        image: TheCocktailImage,
+        name: 'The Cocktail',
+        technoUsed: [
+            ReactIcon,
+            AntDesignIcon,
+        ],
+        url: 'https://diego-1d.github.io/the-cocktail/'
+    },
+    {
+        id: 4,
+        image: FinancesControlImage,
+        name: 'Finances Control',
+        technoUsed: [
+            ReactIcon,
+            TypeScriptIcon,
+            FirebaseIcon,
+            StyledComponentsIcon
+        ],
+        url: 'https://diego-1d.github.io/finances-control/'
+    },
+    {
+        id: 5,
+        image: IceCreamImage,
+        name: 'Menu Ice Cream ',
+        technoUsed: [
+            ReactIcon,
+            StyledComponentsIcon
+        ],
+        url: 'https://diego-1d.github.io/menu-icecream/'
+    },
+    {
+        id: 6,
+        image: FitSpaImage,
+        name: 'Fit Spa',
+        technoUsed: [
+            ReactIcon,
+            StyledComponentsIcon
+        ],
+        url: 'https://diego-1d.github.io/fit-spa/'
+    },
+
+];
