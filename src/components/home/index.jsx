f7594cfdb622c613618de0466ba93e4dcd00514b
Diego-1D <diego1d_fernandes@outlook.com>
2022-08-17 13:08:58 -0300

    refactor: (navbar component) create navbar component

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -1,38 +1,57 @@
 import React from 'react'
-import { AppBar, Box, CardMedia, Container, Typography } from '@mui/material'
+import { Description } from '../../theme/styles'
+import { AppBar, Box, Button, CardMedia, Container, Typography } from '@mui/material'
 import profileImg from '../../Images/profile.svg'
 
 const Home = () => {
     return (
-        <AppBar position="static" sx={{ bgcolor: '#161E35' }}>
+        <AppBar position='static' sx={{ bgcolor: '#161E35' }}>
             <Container>
                 <Box
                     sx={{
                         display: { xs: 'block', md: 'flex' },
                         justifyContent: 'center',
                         alignItems: 'center',
-                        paddingY: '3rem',
+                        paddingY: '3.5rem',
                     }}>
-                    <Box sx={{ paddingRight:{ xs: 0, md: 15 }}}>
-                        <Typography variant='h3' sx={{ fontWeight: 700, color: '#CCC' }}>
+                    <Box
+                        sx={{
+                            flex: 0.8,
+                            display: 'flex',
+                            flexDirection: 'column',
+                            gap: '1rem',
+                            paddingRight: { xs: 0, md: 5 },
+                        }}>
+                        <Typography variant='h3' sx={{ fontWeight: 700, color: '#ACABBD' }}>
                             Olá, Eu sou um<br />
                             <span style={{ color: '#FFF' }}>Desenvolvedor Front-End</span>
                         </Typography>
-                        <Typography variant='h6' color='#FFF' sx={{ paddingTop: '0.8rem' }}>
+                        <Description>
                             Profissional formado na área de Sistemas de Informação,
                             com experiência na área de desenvolvimento de Front-End para aplicações Web.
-                        </Typography>
+                        </Description>
+                        <Button
+                            sx={{
+                                width: '8rem',
+                                backgroundColor: '#F8032D',
+                                color: '#FFF',
+                                borderRadius: '18px'
+                            }}
+                        >
+                            Contate-me
+                        </Button>
                     </Box>
                     <Box
-                       sx={{
-                        display: 'flex',
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                    }}>
+                        sx={{
+                            flex: 0.6,
+                            display: 'flex',
+                            justifyContent: 'center',
+                            alignItems: 'center'
+                        }}>
                         <CardMedia
-                            component="img"
+                            component='img'
                             image={profileImg}
-                            alt="Paella dish"
+                            alt='Paella dish'
                             sx={{ objectFit: 'cover', width: '320px' }}
                         />
                     </Box>
