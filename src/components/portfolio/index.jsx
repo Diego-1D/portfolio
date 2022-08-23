0eca8c9c23a7e9460b975993dac20f23f73c3650
Diego-1D <diego1d_fernandes@outlook.com>
2022-08-23 18:54:24 -0300

    refactor: (portfolio component)

--- src/components/portfolio/index.jsx
+++ src/components/portfolio/index.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import { Projects } from '../../data/projects'
+import React, { useEffect, useState } from 'react'
+import projectsList from '../../utils/projects'
 import { Description, Title } from '../../theme/styles'
 import {
     AppBar,
@@ -7,33 +7,68 @@ import {
     Card,
     CardMedia,
     Container,
+    Pagination,
+    Stack,
 } from '@mui/material'
 
+const cardImage = {
+    flex: '1 1 300px',
+    display: 'flex',
+    flexDirection: 'column',
+    margin: '25px',
+    border: '1px solid transparent',
+    backgroundColor: 'transparent',
+    transition: 'all 0.3s ease-in-out',
+    cursor: 'pointer',
+
+    ":hover": {
+        border: '1px solid #F8032D',
+        transition: 'all 0.3s ease-in-out',
+    }
+}
+
+const pageSize = 6
+
 const Portfolio = () => {
+
+    const [projects, setProjects] = useState([])
+    const [pagination, setPagination] = useState({
+        count: 0,
+        from: 0,
+        to: pageSize
+    })
+
+    useEffect(() => {
+        projectsList.getData({ from: pagination.from, to: pagination.to }).then(response => {
+            setPagination({ ...pagination, count: response.count })
+            setProjects(response.data)
+        })
+    }, [pagination.from, pagination.to])
+
+    const handlePageChange = (event, page) => {
+        const from = (page - 1) * pageSize
+        const to = (page - 1) * pageSize + pageSize
+
+        setPagination({ ...pagination, from: from, to: to })
+    }
+
+
     return (
-        <AppBar position="static" sx={{ bgcolor: '#161E35' }}>
-            <Container sx={{ paddingY: '1.5rem' }}>
+        <AppBar id='portfolio' position="static" sx={{ bgcolor: '#161E35', boxShadow: 0 }}>
+            <Container sx={{
+                minHeight: 'auto',
+                paddingY: '1.5rem'
+            }}>
                 <Title>Portfólio</Title>
-                <Box
-                    sx={{
-                        height: 'auto',
-                        display: 'flex',
-                        justifyContent: 'center',
-                        flexWrap: 'wrap',
-                    }}>
-                    {Projects.map((index) => (
+                <Box sx={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    flexWrap: 'wrap',
+                }}>
+                    {projects.map((index) => (
                         <Card
                             key={index.id}
-                            sx={{
-                                flex: '1 1 300px',
-                                display: 'flex',
-                                flexDirection: 'column',
-                                margin: '20px',
-                                border: '2px solid transparent',
-                                backgroundColor: 'transparent',
-                                transition: 'all 0.5s ease-in-out',
-                                gap: '0.3rem',
-                            }}>
+                            sx={cardImage}>
                             <CardMedia
                                 component="img"
                                 height="180"
@@ -52,10 +87,22 @@ const Portfolio = () => {
                                         }}
                                     />
                                 ))}
-                            </  Box >
+                            </Box >
                         </Card>
                     ))}
                 </Box>
+                <Stack spacing={2}
+                    sx={{
+                        display: 'flex',
+                        alignItems: 'center',
+                    }}>
+                    <Pagination
+                        count={Math.ceil(pagination.count / pageSize)}
+                        onChange={handlePageChange}
+                        shape="rounded"
+                        color="error"
+                    />
+                </Stack>
             </Container>
         </AppBar>
     )
