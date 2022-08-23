c545087bcc0792c92ab6140ab862e397ed1a4e02
Diego-1D <diego1d_fernandes@outlook.com>
2022-08-23 18:50:13 -0300

    feat: (technologies data) inserts data from the technologies used

---
+++ src/data/technologies.js
@@ -0,0 +1,67 @@
+export const Technologies = [
+    {
+        id: 1,
+        icon: 'https://cdn.worldvectorlogo.com/logos/javascript-1.svg',
+        name: 'JavaScript'
+    },
+    {
+        id: 2,
+        icon: 'https://cdn.worldvectorlogo.com/logos/html-1.svg',
+        name: 'Html'
+    },
+    {
+        id: 3,
+        icon: 'https://cdn.worldvectorlogo.com/logos/react-2.svg',
+        name: 'React'
+    },
+    {
+        id: 4,
+        icon: 'https://cdn.worldvectorlogo.com/logos/nextjs-2.svg',
+        name: 'NextJs'
+    },
+    {
+        id: 5,
+        icon: 'https://cdn.worldvectorlogo.com/logos/typescript-2.svg',
+        name: 'TypeScript'
+    },
+    {
+        id: 6,
+        icon: 'https://cdn.worldvectorlogo.com/logos/css-3.svg',
+        name: 'Css'
+    },
+    {
+        id: 7,
+        icon: 'https://cdn.worldvectorlogo.com/logos/styled-components-1.svg',
+        name: 'Styled Components'
+    },
+    {
+        id: 8,
+        icon: 'https://cdn.worldvectorlogo.com/logos/sass-1.svg',
+        name: 'Sass'
+    },
+    {
+        id: 9,
+        icon: 'https://seeklogo.com/images/A/ant-design-logo-EAB6B3D5D9-seeklogo.com.png',
+        name: 'Ant Design'
+    },
+    {
+        id: 10,
+        icon: 'https://cdn.worldvectorlogo.com/logos/tailwindcss.svg',
+        name: 'TailwindCss'
+    },
+    {
+        id: 11,
+        icon: 'https://cdn.worldvectorlogo.com/logos/bootstrap-5-1.svg',
+        name: 'BootStrap'
+    },
+    {
+        id: 12,
+        icon: 'https://cdn.worldvectorlogo.com/logos/firebase-2.svg',
+        name: 'Firebase'
+    },
+    {
+        id: 13,
+        icon: 'https://cdn.worldvectorlogo.com/logos/material-ui-1.svg',
+        name: 'Material Ui'
+    },
+];
