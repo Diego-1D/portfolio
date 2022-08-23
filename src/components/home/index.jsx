9324e12f7eff7e2d5d13416035fb4f7f68319e0d
Diego-1D <diego1d_fernandes@outlook.com>
2022-08-23 18:55:24 -0300

    refactor: (home component)

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -2,73 +2,67 @@ import React from 'react'
 import { Description } from '../../theme/styles'
 import { AppBar, Box, Button, CardMedia, Container, Typography } from '@mui/material'
 import profileImg from '../../Images/profile.svg'
+import { theme } from '../../theme/muiTheme'
+
+const container = {
+    display: 'flex',
+    flexDirection: { xs: 'column-reverse', md: 'row' },
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingTop: '7rem',
+}
 
 const Home = () => {
     return (
         <AppBar id='home' position='static' sx={{ bgcolor: '#161E35', boxShadow: 0 }}>
-            <Container>
-                <Box
-                    sx={{
-                        display: 'flex',
-                        flexDirection: { xs: 'column-reverse', md: 'row' },
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                        paddingTop: '7rem',
-                        paddingBottom: '2rem'
+            <Container sx={container}>
+                <Box sx={{
+                    flex: 0.8,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    gap: '1rem',
+                    paddingRight: { xs: 0, md: 5 },
+                }}>
+                    <Typography sx={{
+                        fontSize: { xs: '2rem', sm: '2.4rem', md: '2.8rem' },
+                        fontWeight: 700,
+                        lineHeight: '3.2rem',
+                        color: theme.palette.grey
                     }}>
-                    <Box
+                        Olá, Eu sou um<br />
+                        <span style={{ color: theme.palette.light }}>Desenvolvedor Front-End</span>
+                    </Typography>
+                    <Description>
+                        Profissional formado na área de Sistemas de Informação,
+                        com experiência na área de desenvolvimento de Front-End para aplicações Web.
+                    </Description>
+                    <Button sx={{
+                        width: '8rem',
+                        backgroundColor: theme.palette.erro,
+                        color: theme.palette.light,
+                        borderRadius: '18px'
+                    }}>
+                        Contate-me
+                    </Button>
+                </Box>
+                <Box sx={{
+                    flex: 0.6,
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    width: { xs: '250px', md: '380px' },
+                    height: { xs: '250px', md: '380px' }
+                }}>
+                    <CardMedia
+                        component='img'
+                        image={profileImg}
+                        alt='Paella dish'
                         sx={{
-                            flex: 0.8,
-                            display: 'flex',
-                            flexDirection: 'column',
-                            gap: '1rem',
-                            paddingRight: { xs: 0, md: 5 },
-                        }}>
-                        <Typography
-                            sx={{
-                                fontSize: { xs: '2rem', sm: '2.4rem', md: '2.8rem' },
-                                fontWeight: 700,
-                                lineHeight: '3.2rem',
-                                color: '#ACABBD'
-                            }}>
-                            Olá, Eu sou um<br />
-                            <span style={{ color: '#FFF' }}>Desenvolvedor Front-End</span>
-                        </Typography>
-                        <Description>
-                            Profissional formado na área de Sistemas de Informação,
-                            com experiência na área de desenvolvimento de Front-End para aplicações Web.
-                        </Description>
-                        <Button
-                            sx={{
-                                width: '8rem',
-                                backgroundColor: '#F8032D',
-                                color: '#FFF',
-                                borderRadius: '18px'
-                            }}
-                        >
-                            Contate-me
-                        </Button>
-                    </Box>
-                    <Box
-                        sx={{
-                            flex: 0.6,
-                            display: 'flex',
-                            justifyContent: 'center',
-                            alignItems: 'center',
-                            width: { xs: '250px', md: '380px' },
-                            height: { xs: '250px', md: '380px' }
-                        }}>
-                        <CardMedia
-                            component='img'
-                            image={profileImg}
-                            alt='Paella dish'
-                            sx={{
-                                height: '100%',
-                                width: '100%',
-                                objectFit: 'contain'
-                            }}
-                        />
-                    </Box>
+                            height: '100%',
+                            width: '100%',
+                            objectFit: 'contain'
+                        }}
+                    />
                 </Box>
             </Container>
         </AppBar>
