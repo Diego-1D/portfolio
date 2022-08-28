0abca8b4ed2eb5791bebfb12ad677215384d4454
Diego-1D <diego1d_fernandes@outlook.com>
2022-08-28 18:09:27 -0300

    refactor: (experiences) changes the folder name to experience

---
+++ src/components/experiences/index.jsx
@@ -0,0 +1,69 @@
+import React from 'react'
+import { AppBar, Box, Container, Typography } from '@mui/material'
+import { Description, SubTitle, Title } from '../../theme/styles'
+import { theme } from '../../theme/muiTheme'
+
+const wrapper = {
+    display: 'flex',
+    flexDirection: 'column',
+    paddingTop: '1.5rem'
+}
+
+const Experiences = () => {
+    return (
+        <Container sx={{
+            minHeight: 'auto',
+            paddingTop: '1rem',
+            paddingBottom: '2rem'
+        }}>
+            <Title>Experiências</Title>
+            <Box sx={wrapper}>
+                <Typography
+                    variant='h6'
+                    sx={{
+                        color: theme.palette.light,
+                        fontWeight: 'bold'
+                    }}>
+                    Acadêmica
+                </Typography>
+                <SubTitle>Universidade:
+                    <Description> Centro Universitário Luterano de Santarém · Ceuls/Ulbra</Description>
+                </SubTitle>
+                <SubTitle>Curso:
+                    <Description> Bacharelado em Sistemas de Informação </Description>
+                </SubTitle>
+                <SubTitle>Período:
+                    <Description> Agosto de 2017 · Junho de 2021 </Description>
+                </SubTitle>
+            </Box>
+            <Box sx={wrapper}>
+                <Typography
+                    variant='h6'
+                    sx={{
+                        color: theme.palette.light,
+                        fontWeight: 'bold'
+                    }}>
+                    Profissional
+                </Typography>
+                <SubTitle>Empresa:
+                    <Description>Tracktus</Description>
+                </SubTitle>
+                <SubTitle>Localidade:
+                    <Description> Rio Grande do Sul, Brasil · Remoto</Description>
+                </SubTitle>
+                <SubTitle>Cargo: <Description> Desenvolvedor de front-end · Freelance</Description>
+                </SubTitle>
+                <SubTitle>Período:
+                    <Description> Jun de 2022 - Jul de 2022 · 2 meses</Description>
+                </SubTitle>
+                <SubTitle>Descrição:
+                    <Description> Desenvolvimento de aplicações web utilizando o NextJS, React e Material UI.</Description>
+                </SubTitle>
+                <SubTitle>Competências: <Description> Next.js · React.js · JavaScript/TypeScript · Material-UI</Description>
+                </SubTitle>
+            </Box>
+        </Container>
+    )
+}
+
+export default Experiences
