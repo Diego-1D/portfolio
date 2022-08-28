812de6f6a1ad0f8ba27da4f318cd63e2fa25a29c
Diego-1D <diego1d_fernandes@outlook.com>
2022-08-28 18:28:38 -0300

    global styles

--- src/theme/muiTheme.js
+++ src/theme/muiTheme.js
@@ -1,7 +1,14 @@
 import { createTheme } from "@mui/material";
 
 export const theme = createTheme({
+  shadows: 0,
+  typography: {
+    fontFamily: 'Poppins, sans-serif'
+  },
   palette: {
+    background: {
+      default:'#161E35'
+    },
     light: '#FFFFFF',
     grey: '#ACABBD',
     erro: '#F8032D'
--- src/theme/styles.js
+++ src/theme/styles.js
@@ -1,22 +1,24 @@
 import { styled } from "@mui/system";
-import { theme } from "./muiTheme";
 
-export const Title = styled("h1")(({}) => ({
+export const Title = styled("h1")(({ theme }) => ({
+    color: theme.palette.light,
     fontFamily: 'Poppins, sans-serif',
     fontWeight: "700",
     fontSize: "1.4rem",
     textDecoration: 'underline',
-    textDecorationColor: theme.palette.erro
+    textDecorationColor: theme.palette.erro,
+    margin: '0'
 }));
 
-export const Description = styled("subtitle")(({}) => ({
+export const Description = styled("subtitle")(({ theme }) => ({
     fontFamily: 'Poppins, sans-serif',
     fontSize: "1rem",
     color: theme.palette.grey,
     lineHeight: '1.8rem'
 }));
 
-export const SubTitle = styled("subtitle")(({}) => ({
+export const SubTitle = styled("subtitle")(({ theme }) => ({
+    color: theme.palette.light,
     fontFamily: 'Poppins, sans-serif',
     fontSize: "1rem",
 }));
