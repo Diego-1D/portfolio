46a85f945eccc9885688a1f20ed5b3333f380b71
Diego-1D <diego1d_fernandes@outlook.com>
2022-09-20 17:41:41 -0300

    adding a new project

--- src/data/projects.js
+++ src/data/projects.js
@@ -22,6 +22,17 @@ export const Projects = [
     },
     {
         key: 3,
+        image: "https://raw.githubusercontent.com/Diego-1D/pomo-task/main/src/assets/hero.png",
+        name: 'Pomo Task',
+        technoUsed: [
+            'https://cdn.worldvectorlogo.com/logos/react-2.svg',
+            'https://cdn.worldvectorlogo.com/logos/typescript-2.svg',
+            'https://cdn.worldvectorlogo.com/logos/sass-1.svg'
+        ],
+        url: 'https://diego-1d.github.io/pomo-task/'
+    },
+    {
+        key: 4,
         image: 'https://raw.githubusercontent.com/Diego-1D/the-cocktail/main/src/github/img.png',
         name: 'The Cocktail',
         technoUsed: [
@@ -31,7 +42,7 @@ export const Projects = [
         url: 'https://diego-1d.github.io/the-cocktail/'
     },
     {
-        key: 4,
+        key: 5,
         image: 'https://raw.githubusercontent.com/Diego-1D/finances-control/main/src/git/img.png',
         name: 'Finances Control',
         technoUsed: [
@@ -43,68 +54,7 @@ export const Projects = [
         url: 'https://diego-1d.github.io/finances-control/'
     },
     {
-        key: 5,
-        image: 'https://github.com/Diego-1D/menu-icecream/raw/main/src/images/HeroImg.png?raw=true',
-        name: 'Menu Ice Cream ',
-        technoUsed: [
-            'https://cdn.worldvectorlogo.com/logos/react-2.svg',
-            'https://cdn.worldvectorlogo.com/logos/styled-components-1.svg'
-        ],
-        url: 'https://diego-1d.github.io/menu-icecream/'
-    },
-    {
         key: 6,
-        image: 'https://raw.githubusercontent.com/Diego-1D/fit-spa/master/src/images/HeroImg.png',
-        name: 'Fit Spa',
-        technoUsed: [
-            'https://cdn.worldvectorlogo.com/logos/react-2.svg',
-            'https://cdn.worldvectorlogo.com/logos/styled-components-1.svg'
-        ],
-        url: 'https://diego-1d.github.io/fit-spa/'
-    },
-    {
-        key: 7,
-        image: "https://github.com/Diego-1D/crm-dashboard/blob/main/src/assets/images/hero-git.png?raw=true",
-        name: 'CRM Dashboard',
-        technoUsed: [
-            'https://cdn.worldvectorlogo.com/logos/react-2.svg',
-            'https://cdn.worldvectorlogo.com/logos/styled-components-1.svg'
-        ],
-        url: 'https://diego-1d.github.io/crm-dashboard/'
-    },
-    {
-        key: 8,
-        image: "https://raw.githubusercontent.com/Diego-1D/pomo-task/main/src/assets/hero.png",
-        name: 'Pomo Task',
-        technoUsed: [
-            'https://cdn.worldvectorlogo.com/logos/react-2.svg',
-            'https://cdn.worldvectorlogo.com/logos/typescript-2.svg',
-            'https://cdn.worldvectorlogo.com/logos/sass-1.svg'
-        ],
-        url: 'https://diego-1d.github.io/pomo-task/'
-    },
-    {
-        key: 9,
-        image: "https://github.com/Diego-1D/ecommerce-app/blob/main/src/assets/git_home.png?raw=true",
-        name: 'Shopping',
-        technoUsed: [
-            'https://cdn.worldvectorlogo.com/logos/react-2.svg',
-            'https://cdn.worldvectorlogo.com/logos/bootstrap-5-1.svg'
-        ],
-        url: ''
-    },
-    {
-        key: 10,
-        image: "https://raw.githubusercontent.com/Diego-1D/bempaggo_form/main/src/assets/imgs/hero.png",
-        name: 'Formulário Bem Paggo',
-        technoUsed: [
-            'https://cdn.worldvectorlogo.com/logos/react-2.svg',
-            'https://cdn.worldvectorlogo.com/logos/styled-components-1.svg'
-        ],
-        url: 'https://diego-1d.github.io/bempaggo_form/'
-    },
-    {
-        key: 11,
         image: "https://raw.githubusercontent.com/Diego-1D/food-delivery/38ddc59ff5bc8b047de73643820ce9ed7a946101/src/img/git_hero.png",
         name: 'Food Delivery',
         technoUsed: [
@@ -114,4 +64,64 @@ export const Projects = [
         ],
         url: 'https://github.com/Diego-1D/food-delivery/'
     },
+    {
+        key: 7,
+        image: 'https://github.com/Diego-1D/booking-clone/blob/main/src/img_git.png?raw=true',
+        name: 'Booking Clone',
+        technoUsed: [
+            'https://cdn.worldvectorlogo.com/logos/react-2.svg',
+            'https://cdn.worldvectorlogo.com/logos/material-ui-1.svg'
+        ],
+        url: 'https://diego-1d.github.io/booking-clone/'
+    },
+    {
+        key: 8,
+        image: 'https://github.com/Diego-1D/menu-icecream/raw/main/src/images/HeroImg.png?raw=true',
+        name: 'Menu Ice Cream',
+        technoUsed: [
+            'https://cdn.worldvectorlogo.com/logos/react-2.svg',
+            'https://cdn.worldvectorlogo.com/logos/styled-components-1.svg'
+        ],
+        url: 'https://diego-1d.github.io/menu-icecream/'
+    },
+    {
+        key: 9,
+        image: 'https://raw.githubusercontent.com/Diego-1D/fit-spa/master/src/images/HeroImg.png',
+        name: 'Fit Spa',
+        technoUsed: [
+            'https://cdn.worldvectorlogo.com/logos/react-2.svg',
+            'https://cdn.worldvectorlogo.com/logos/styled-components-1.svg'
+        ],
+        url: 'https://diego-1d.github.io/fit-spa/'
+    },
+    {
+        key: 10,
+        image: "https://github.com/Diego-1D/crm-dashboard/blob/main/src/assets/images/hero-git.png?raw=true",
+        name: 'CRM Dashboard',
+        technoUsed: [
+            'https://cdn.worldvectorlogo.com/logos/react-2.svg',
+            'https://cdn.worldvectorlogo.com/logos/styled-components-1.svg'
+        ],
+        url: 'https://diego-1d.github.io/crm-dashboard/'
+    },
+    {
+        key: 11,
+        image: "https://github.com/Diego-1D/ecommerce-app/blob/main/src/assets/git_home.png?raw=true",
+        name: 'Shopping',
+        technoUsed: [
+            'https://cdn.worldvectorlogo.com/logos/react-2.svg',
+            'https://cdn.worldvectorlogo.com/logos/bootstrap-5-1.svg'
+        ],
+        url: ''
+    },
+    {
+        key: 12,
+        image: "https://raw.githubusercontent.com/Diego-1D/bempaggo_form/main/src/assets/imgs/hero.png",
+        name: 'Formulário Bem Paggo',
+        technoUsed: [
+            'https://cdn.worldvectorlogo.com/logos/react-2.svg',
+            'https://cdn.worldvectorlogo.com/logos/styled-components-1.svg'
+        ],
+        url: 'https://diego-1d.github.io/bempaggo_form/'
+    },  
 ];
